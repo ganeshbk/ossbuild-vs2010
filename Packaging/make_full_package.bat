60451fd35fd6f259b094b7c5f58cb93a7a243d70
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2012-05-29 12:31:34 +0300

    Modified packaging script to skip some unnecessary files.

--- Packaging/make_full_package.bat
+++ Packaging/make_full_package.bat
@@ -12,6 +12,21 @@ copy %BINDIR%\plugins\*.dll %PKGDIR%\bin\plugins
 copy %MYDIR%\example_scripts\* %PKGDIR%
 copy %SHAREDDIR%\Build\Windows\Win32\bin\*.dll %PKGDIR%\lib
 
+:: Python DLLs cause errors if Python is not installed on target computer
+:: Move them to subdir for this reason.
+mkdir %PKGDIR%\bin\python
+move %PKGDIR%\bin\plugins\libgstpython* %PKGDIR%\bin\python
+
+:: Keep all dependencies in lib folder, remove duplicates from bin folder.
+for /f %%i in ('"dir /b %PKGDIR%\lib"') do (
+	if exist "%PKGDIR%\bin\%%i" (
+		del "%PKGDIR%\bin\%%i"
+	)
+)
+
+:: Depending on whether you are using GPL or LGPL build, remove unnecessary libs
+del %PKGDIR%\lib\av*-gpl-*
+
 @echo "Now running basic sanity check"
 cd %PKGDIR%
 .\list_plugins.bat
